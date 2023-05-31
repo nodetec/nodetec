a020d4c97d89fa8e20dc4da3e70c3d7016a44473
Christian Chiarulli <chris.machine@pm.me>
2023-05-31 17:46:02 -0400

    search animations

--- src/components/SearchPopup.tsx
+++ src/components/SearchPopup.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import {
   DocumentTextIcon,
   HashtagIcon,
   Bars3BottomLeftIcon,
 } from "@heroicons/react/24/outline";
-import { Dialog, Combobox } from "@headlessui/react";
+import { Dialog, Combobox, Transition } from "@headlessui/react";
 
 export default function SearchPopup({
   isOpen,
@@ -82,67 +82,64 @@ export default function SearchPopup({
   }
 
   return (
-    <Dialog open={isOpen} className="fixed z-50 top-52" onClose={closePopup}>
-      <div className="fixed inset-0 bg-black/30 backdrop-blur-sm" />
-      <div className="fixed inset-0 p-4 sm:p-8 md:p-28">
-        <Dialog.Panel className="border pb-4 border-slate-300 dark:border-slate-700 mx-auto rounded-lg max-w-3xl bg-slate-50 dark:bg-slate-800 shadow-3xl overflow-hidden">
-          <Combobox
-            onChange={(loc: any) => {
-              window.location = loc;
-              setIsOpen(false);
-            }}
+    <Transition.Root
+      show={isOpen}
+      as={Fragment}
+      afterLeave={() => setQuery("")}
+      appear
+    >
+      <Dialog open={isOpen} className="fixed z-50 top-52" onClose={closePopup}>
+        <div className="fixed inset-0 bg-black/30 backdrop-blur-sm" />
+        <div className="fixed inset-0 p-4 sm:p-8 md:p-28">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
-            <div className="border-b flex items-center mb-4 border-slate-300 dark:border-slate-700">
-              <Combobox.Input
-                className="w-full text-lg border-none p-8 h-8 focus:ring-0 text-slate-900 placeholder:text-slate-400 dark:text-slate-100 placeholder-slate-400 bg-transparent"
-                onChange={(event) => setQuery(event.target.value)}
-                placeholder="Type to search..."
-              />
-              <span className="p-1 select-none items-center text-slate-500 justify-center text-xs font-mono tracking-wide leading-3 pointer-events-none border border-slate-300 dark:border-slate-500 rounded-md mr-8 font-thin">
-                <kbd className="text-inherit" aria-hidden="true">
-                  esc
-                </kbd>
-              </span>
-            </div>
+            <Dialog.Panel className="border pb-4 border-slate-300 dark:border-slate-700 mx-auto rounded-lg max-w-3xl bg-slate-50 dark:bg-slate-800 shadow-3xl overflow-hidden">
+              <Combobox
+                onChange={(loc: any) => {
+                  window.location = loc;
+                  setIsOpen(false);
+                }}
+              >
+                <div className="border-b flex items-center mb-4 border-slate-300 dark:border-slate-700">
+                  <Combobox.Input
+                    className="w-full text-lg border-none p-8 h-8 focus:ring-0 text-slate-900 placeholder:text-slate-400 dark:text-slate-100 placeholder-slate-400 bg-transparent"
+                    onChange={(event) => setQuery(event.target.value)}
+                    placeholder="Type to search..."
+                  />
+                  <span className="p-1 select-none items-center text-slate-500 justify-center text-xs font-mono tracking-wide leading-3 pointer-events-none border border-slate-300 dark:border-slate-500 rounded-md mr-8 font-thin">
+                    <kbd className="text-inherit" aria-hidden="true">
+                      esc
+                    </kbd>
+                  </span>
+                </div>
 
-            {query === "" && <NoResults />}
-            {query !== "" && transformedContent.length === 0 && <NoResults />}
+                {query === "" && <NoResults />}
+                {query !== "" && transformedContent.length === 0 && (
+                  <NoResults />
+                )}
 
-            {transformedContent.length > 0 && (
-              <Combobox.Options className="px-4 pb-4 max-h-[30rem] scroll-p-12 space-y-2 overflow-y-auto">
-                {transformedContent.map((content: any) => (
-                  <li key={content.slug}>
-                    <div className=" pl-1 py-2 gap-2 flex items-center">
-                      <DocumentTextIcon className="h-7 w-7 text-slate-600 dark:text-slate-300" />
-                      <h2 className="font-semibold text-slate-600 dark:text-slate-300">
-                        {content.label}
-                      </h2>
-                    </div>
-                    <ul className="text-slate-600 dark:text-slate-300 font-semibold">
-                      {content.content.map((section: any) => (
-                        <div>
-                          <Combobox.Option
-                            key={section.header}
-                            className={({ active }) =>
-                              `${
-                                active &&
-                                "bg-slate-100 text-slate-900 dark:bg-slate-700 dark:text-slate-100 rounded-lg"
-                              }`
-                            }
-                            value={`/${content.slug}#${cleanHeaderLink(
-                              section.header
-                            )}`}
-                          >
-                            <div className="cursor-pointer border-l-2 border-slate-400 ml-4 pl-4 py-4 gap-2 flex items-center">
-                              <HashtagIcon className="h-6 w-6" />
-                              <p>{section.header}</p>
-                            </div>
-                          </Combobox.Option>
-
-                          {section.paragraphs.map(
-                            (paragraph: any, index: any) => (
+                {transformedContent.length > 0 && (
+                  <Combobox.Options className="px-4 pb-4 max-h-[30rem] scroll-p-12 space-y-2 overflow-y-auto">
+                    {transformedContent.map((content: any) => (
+                      <li key={content.slug}>
+                        <div className=" pl-1 py-2 gap-2 flex items-center">
+                          <DocumentTextIcon className="h-7 w-7 text-slate-600 dark:text-slate-300" />
+                          <h2 className="font-semibold text-slate-600 dark:text-slate-300">
+                            {content.label}
+                          </h2>
+                        </div>
+                        <ul className="text-slate-600 dark:text-slate-300 font-semibold">
+                          {content.content.map((section: any) => (
+                            <div>
                               <Combobox.Option
-                                key={index}
+                                key={section.header}
                                 className={({ active }) =>
                                   `${
                                     active &&
@@ -154,22 +151,44 @@ export default function SearchPopup({
                                 )}`}
                               >
                                 <div className="cursor-pointer border-l-2 border-slate-400 ml-4 pl-4 py-4 gap-2 flex items-center">
-                                  <Bars3BottomLeftIcon className="h-6 w-6 " />
-                                  <p>{paragraph.slice(0, 75)}</p>
+                                  <HashtagIcon className="h-6 w-6" />
+                                  <p>{section.header}</p>
                                 </div>
                               </Combobox.Option>
-                            )
-                          )}
-                        </div>
-                      ))}
-                    </ul>
-                  </li>
-                ))}
-              </Combobox.Options>
-            )}
-          </Combobox>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+
+                              {section.paragraphs.map(
+                                (paragraph: any, index: any) => (
+                                  <Combobox.Option
+                                    key={index}
+                                    className={({ active }) =>
+                                      `${
+                                        active &&
+                                        "bg-slate-100 text-slate-900 dark:bg-slate-700 dark:text-slate-100 rounded-lg"
+                                      }`
+                                    }
+                                    value={`/${content.slug}#${cleanHeaderLink(
+                                      section.header
+                                    )}`}
+                                  >
+                                    <div className="cursor-pointer border-l-2 border-slate-400 ml-4 pl-4 py-4 gap-2 flex items-center">
+                                      <Bars3BottomLeftIcon className="h-6 w-6 " />
+                                      <p>{paragraph.slice(0, 75)}</p>
+                                    </div>
+                                  </Combobox.Option>
+                                )
+                              )}
+                            </div>
+                          ))}
+                        </ul>
+                      </li>
+                    ))}
+                  </Combobox.Options>
+                )}
+              </Combobox>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
   );
 }
