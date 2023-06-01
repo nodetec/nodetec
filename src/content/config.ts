ec733f53340bd702dfce58d30db1f1ac38ac6eba
Christian Chiarulli <chris.machine@pm.me>
2023-06-01 02:44:59 -0400

    cleanup

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,7 @@ const docs = defineCollection({
     title: z.string().default(SITE.title),
     description: z.string().default(SITE.description),
     lang: z.literal("en-us").default(SITE.defaultLanguage),
-    image: z.string().default(OPEN_GRAPH.image.src),
+    image: z.string().optional().default(OPEN_GRAPH.image.src),
     ogLocale: z.string().optional(),
   }),
 });
